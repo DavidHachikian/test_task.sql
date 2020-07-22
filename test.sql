b99efb867d2372fa3c801370302307289f5276bb
DavidHachikian <57405844+DavidHachikian@users.noreply.github.com>
2020-07-22 15:41:23 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,13 +1,16 @@
-1) SELECT * 
+1) Вывести список сотрудников, получающих максимальную заработную плату в своем отделе
+SELECT * 
 FROM Employee AS employees
 WHERE employees.salary = (SELECT MAX(salary) FROM Employee AS max WHERE max.department_id = employees.department_id);
 
-2) SELECT department_id 
+2) Вывести список ID отделов, количество сотрудников в которых не превышает 3 человек
+SELECT department_id 
 FROM Employee
 GROUP BY department_id
 HAVING COUNT(*) <= 3;
 
-3) WITH dep_salary AS 
+3) Найти список ID отделов с максимальной суммарной зарплатой сотрудников
+WITH dep_salary AS 
 	(SELECT department_id, sum(salary) AS salary
 	FROM employee 
 	GROUP BY department_id)
@@ -15,6 +18,7 @@ SELECT department_id
 FROM dep_salary
 WHERE dep_salary.salary = (SELECT max(salary) FROM dep_salary);
 
-4) INSERT Department (ID, Number)  
+4) Добавить в БД новый отдел
+INSERT Department (ID, Number)  
     VALUES (2, 'New Department')  
 GO 
